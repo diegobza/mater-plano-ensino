e8ceb8cb807bc128910af01a99ef8be709ccae69
Diego Bezerra <diego@tatulab.com>
2015-12-21 16:10:40 -0300

    Correção da referência do favicon..

--- layout/essen.php
+++ layout/essen.php
@@ -71,7 +71,7 @@ function cabecalho()
     echo "    <meta charset=\"utf-8\">\n";
     echo "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
     echo "    <title>Nossa Mater - Plano de Ensino</title>\n";
-    echo "    <link rel=\"icon\" href=\"favicon.png\">\n";
+    echo "    <link rel=\"icon\" href=\"/pe/favicon.png\">\n";
     echo "    <link rel=\"stylesheet\" type=\"text/css\" href=\"https://fonts.googleapis.com/css?family=Exo+2\">\n";
     echo "    <link rel=\"stylesheet\" type=\"text/css\" href=\"/pe/layout/css/bootstrap.min.css\">\n";
     echo "    <link rel=\"stylesheet\" type=\"text/css\" href=\"/pe/layout/css/font-awesome.min.css\">\n";
