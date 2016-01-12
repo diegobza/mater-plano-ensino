2ce057911d1d888da5258bab69a7964d25fbaf36
Diego Bezerra <diego@tatulab.com>
2016-01-12 14:51:04 -0300

    Post.

--- login/index.php
+++ login/index.php
@@ -4,14 +4,19 @@ require '../layout/essen.php';
 
 session_start();
 
+$login = htmlspecialchars($_POST['usuario']);
+$senha = htmlspecialchars($_POST['senha']);
+
 cabecalho();
+
+echo $login . "\n";
 
 echo <<< 'HTML'
         <h2>Acesso ao Sistema</h2>
-        <form class="col-sm-offset-3 col-sm-6 text-left" method="post">
+        <form class="col-sm-offset-3 col-sm-6 text-left" action="index.php" method="post">
             <div class="form-group">
                 <label for="usuario">Usuário</label>
-                <input type="text" class="form-control" id="usuario" placeholder="Digite seu nome de usuário">
+                <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Digite seu nome de usuário">
             </div>
             <div class="form-group">
                 <label for="senha">Senha</label>
